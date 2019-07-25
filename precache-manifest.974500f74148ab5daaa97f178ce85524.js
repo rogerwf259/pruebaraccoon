5d63b7ca2c60af901f16a957dd87fb43457ac8c2
Roger <rogeliomer_2@hotmail.com>
2019-07-24 21:31:46 -0500

    Updates

---
+++ precache-manifest.974500f74148ab5daaa97f178ce85524.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f94d9951fb9bded8e6a5689e75bd98",
+    "url": "/pruebaraccoon/index.html"
+  },
+  {
+    "revision": "6fe17a71ea820f622eb1",
+    "url": "/pruebaraccoon/static/js/2.fbd17c61.chunk.js"
+  },
+  {
+    "revision": "9c6b12807f3605f7673e",
+    "url": "/pruebaraccoon/static/js/main.a79e7046.chunk.js"
+  },
+  {
+    "revision": "5e3bec6d50641786461b",
+    "url": "/pruebaraccoon/static/js/runtime~main.89c6744c.js"
+  }
+]);
